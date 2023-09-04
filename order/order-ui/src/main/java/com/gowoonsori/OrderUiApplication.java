3b07c4df09e82dbda3dde94266ac106e6759f013
EuiSung <gowoonsori97@naver.com>
2023-09-04 16:33:14 +0900

    Update : gradle 빌드 적용

---
+++ order/order-ui/src/main/java/com/gowoonsori/OrderUiApplication.java
@@ -0,0 +1,11 @@
+package com.gowoonsori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderUiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderUiApplication.class, args);
+    }
+}
